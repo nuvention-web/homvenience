cfa1f2ffbd25ddf3b19fbe30bf931a9822e38a90
maktf <xhhxhhxhh@163.com>
2016-02-01 18:06:52 -0600

    modified search

--- www/js/main.js
+++ www/js/main.js
@@ -185,6 +185,7 @@ var Customer = Parse.Object.extend("Customer" , {
       query.notEqualTo("Owner", currentUser.get("username"));
       query.notEqualTo("Holder", currentUser.id);
     }
+    query.equalTo("State","Available");
     query.find().then(function(result) {
       $scope.search_res = result;
       $scope.$apply();
