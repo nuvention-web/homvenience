42c3eca6bff960a6e78209c0a770b09d57c7ac99
maktf <xhhxhhxhh@163.com>
2016-02-25 23:22:18 -0600

    modified detail page to be dynamic and change index page to sign-in

--- www/js/app.js
+++ www/js/app.js
@@ -143,6 +143,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     // });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/sign-in');
 
 });
